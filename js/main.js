e6e338bfe1bc3d8fdd66ce473c44551b20f3296c
shantam8 <dscharmen@aol.com>
2021-06-29 21:29:57 +0200

    mobile done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
+let btnHamburger = document.querySelector("#btnHamburger");
+let header = document.querySelector("header");
+let body = document.querySelector("body");
 
-
-
-
-function init(){
-    console.log("Hi");
+function toggleMobileMenu() {
+  if (header.classList.contains("isOpen")) {
+    console.log("closing");
+    header.classList.remove("isOpen");
+    body.classList.remove("overflowHidden");
+  } else {
+    console.log("opening");
+    header.classList.add("isOpen");
+    body.classList.add("overflowHidden");
+  }
 }
 
-
-
+function init() {
+  console.log("Hi");
+  btnHamburger.addEventListener("click", toggleMobileMenu);
+}
 
 window.onload = init();
